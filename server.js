a66b5d1ed032e8678660dca6d85573973852b265
Naomi Ware <nware1066@yahoo.com>
2021-06-07 15:05:18 -0600

    Add hardcoded data to server.js

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api/customers', (req, res) => {
+    const customers = [
+        {
+            id: 1,
+            firstName: 'Elliot',
+            lastName: 'Spencer'
+        },
+        {
+            id: 2,
+            firstName: 'Parker',
+            lastName: 'Parker'
+        },
+        {
+            id: 3,
+            firstName: 'Alec',
+            lastName: 'Hardison'
+        }
+    ];
+    res.json(customers);
+});
+
+
 const port = 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
-
